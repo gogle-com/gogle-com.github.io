621c4c1ee9a7497ded0e6a88a4d7f3d7bb145663
JS Animations <80853499+JS-Animatios@users.noreply.github.com>
2022-12-18 21:22:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var counter = 0
 alert(counter)
 function gmailclick(){
@@ -39,6 +40,13 @@ function micclick(){
 function luckyclick(){
     textvalue = document.getElementById("textbox").value;
     if (textvalue=="$options+new%#" & counter==7){
-        alert("gg")
+        alert("ERROR(code:$options+new%#) ERR CD:302 ADMINACCESSDENIED_3242 ERRDASHVALUE:laubfyg4387gfbkahu7gyb3kg#$TAG$TGSAFZDAG$#GASDFE$G$#EWGEWGWGUYBV&#WRGBVG#UGBEG*EBGIEUWBGE*G&@#FGEJBVNGEIRG*VER*(GHERJ?GVBERUIOGVER*GYW#RHGHERGQUERGB*UWGOQRBENQRIOGU*HEQAW&$*GUAHWGFIUH$*WTGRUGIFU)ERRSOLU:contact google support :)")
+        x.style.display= "block";
     }
+}
+function backcmd(){
+    x.style.display= "none";
+    counter=0
+    document.getElementById("textbox").value = ""; 
+    
 }
