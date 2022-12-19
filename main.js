2be5f684ab1b5643828848ce9a2a6a7ad8a1302b
JS Animations <80853499+JS-Animatios@users.noreply.github.com>
2022-12-18 21:25:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,34 +4,34 @@ alert(counter)
 function gmailclick(){
     if (counter==0){
         counter = 1;
-        alert(counter);
+        //alert(counter);
     }
 }
 function waffleclick(){
     if (counter==1){
         counter = 2;
-        alert(counter);
+        //alert(counter);
     } else if (counter==2){
         counter = 3;
-        alert(counter);
+        //alert(counter);
     }
 }
 function profilepicclick(){
     if (counter==3){
         counter = 4;
-        alert(counter);
+        //alert(counter);
     }
 }
 function termsclick(){
     if (counter==4){
         counter = 5;
-        alert(counter);
+        //alert(counter);
     }
 }
 function micclick(){
     if (counter==5){
         counter = 6;
-        alert(counter);
+        //alert(counter);
     } else if (counter==6){
         counter = 7;
         alert("ERROR(code:$options+new%#) ERR CD:302 ADMINACCESSDENIED_3242 ERRDASHVALUE:laubfyg4387gfbkahu7gyb3kg#$TAG$TGSAFZDAG$#GASDFE$G$#EWGEWGWGUYBV&#WRGBVG#UGBEG*EBGIEUWBGE*G&@#FGEJBVNGEIRG*VER*(GHERJ?GVBERUIOGVER*GYW#RHGHERGQUERGB*UWGOQRBENQRIOGU*HEQAW&$*GUAHWGFIUH$*WTGRUGIFU)ERRSOLU:contact google support :)")
